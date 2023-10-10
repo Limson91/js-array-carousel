f17382fe66a540bfdec957daf06a0356f3c08027
Limson91 <matteo.a.colautti@gmail.com>
2023-10-10 15:53:04 +0200

    add img to array

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // stampa img su dom html
-const images = [];//utilizzare array per immagini o correto richiamarle da html?
+const images = [
+    "./consegna/img/01.webp",
+    "./consegna/img/02.webp"
+];//utilizzare array per immagini o correto richiamarle da html?
 
 //richiamare div="gallery" da html
 const galleryDOMElement = document.querySelector('.gallery');
@@ -14,14 +17,12 @@ for (let i = 0; i <images.length; i++) {
     console.log(currentSRC);
 
     const htmlString = `
-    <div class="image">
-        <img src="${currentSRC}"
-    </div>`
+        <img class="active img" src="${currentSRC}">`
 
     console.log(htmlString);
     galleryDOMElement.innerHTML += htmlString;
 }
 
 const imageDOMElements = document.getElementsByClassName('.img')
-// const imageDOMElements = document.querySelectorAll('.image');
+// const imageDOMElements = document.querySelectorAll('.img');
 console.log(imageDOMElements);
