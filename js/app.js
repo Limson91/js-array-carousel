ab04c5e62cca7902d4677695b4fea3d2d5360252
Limson91 <138766518+Limson91@users.noreply.github.com>
2023-10-12 10:11:11 +0200

    Add css style and js structure for carousel img

--- js/app.js
+++ js/app.js
@@ -1,28 +1,76 @@
-// stampa img su dom html
-const images = [
-    "./consegna/img/01.webp",
-    "./consegna/img/02.webp"
-];//utilizzare array per immagini o correto richiamarle da html?
+console.log('prova test');
 
-//richiamare div="gallery" da html
-const galleryDOMElement = document.querySelector('.gallery');
-console.log(galleryDOMElement);
+const imagesSrc = [
+    './consegna/img/01.webp',
+    './consegna/img/02.webp',
+    './consegna/img/03.webp',
+    './consegna/img/04.webp',
+    './consegna/img/05.webp'
+]
 
-console.log(images);
+const carouselDOMElement = document.querySelector('.carousel'); 
+console.log(carouselDOMElement, imagesSrc);
 
-//ciclo for per utilizzare array
-for (let i = 0; i <images.length; i++) {
-    console.log[i];
-    const currentSRC = images[i];
-    console.log(currentSRC);
+for (let i = 0; i < imagesSrc.length; i++) {
+    console.log(imagesSrc[i]); //stampa elementi array in base a indice
+    const src = imagesSrc[i];
 
-    const htmlString = `
-        <img class="active img" src="${currentSRC}">`
+    const html = `<img class="carousel__item" src="${src}" alt="">`
 
-    console.log(htmlString);
-    galleryDOMElement.innerHTML += htmlString;
+    carouselDOMElement.innerHTML += html;
 }
 
-const imageDOMElements = document.getElementsByClassName('.img')
-// const imageDOMElements = document.querySelectorAll('.img');
-console.log(imageDOMElements);
+//recupero dal DOM tutte le immagini stampate nel ciclo for
+const itemsDOMElements = document.querySelectorAll('.carousel__item');
+console.log(itemsDOMElements);
+
+//memorizzare stato carosello (indice della slide attive)
+let currentIndex = 0;
+// = Math.floor(Math.random() * imagesSrc.length) per scegliere indice random
+
+//aggiungere classe active alla slide attiva
+let currentSlide = itemsDOMElements[currentIndex];
+currentSlide.classList.add('active');
+
+// click su elemento arrow
+const arrowTop = document.querySelector('.carousel .arrow--top');
+const arrowBottom = document.querySelector('.carousel .arrow--bottom');
+console.log(arrowTop, arrowBottom);
+
+arrowBottom.addEventListener('click', function () {
+	console.log('click bottom');
+
+    // prendere la img successiva
+    const NextSlideElement = itemsDOMElements[currentIndex + 1];
+
+    if (NextSlideElement) {
+        // selezionare la img attiva
+    const activeSlideElement = itemsDOMElements[currentIndex];
+    console.log(activeSlideElement);
+	// toglierle la class="active"
+    activeSlideElement.classList.remove('active');
+
+	// aggiungere alla slide successiva la class="active"
+    NextSlideElement.classList.add('active')
+	// incremento currentIndex
+    currentIndex++
+    }
+})
+
+arrowTop.addEventListener('click', function () {
+	console.log('click top')
+
+    //prendo la img precedente
+	const prevSlideElement = itemsDOMElements[currentIndex - 1];
+
+    //se esiste una img precedente, rimuovi la classe attiva dalla img attiva.
+    if (prevSlideElement) {
+        const activeSlideElement = itemsDOMElements[currentIndex];
+        activeSlideElement.classList.remove('active');
+
+        //aggiungo la class='active' alla img precedente
+        prevSlideElement.classList.add('active');
+        //decremento currentIndex
+        currentIndex--
+    }
+})
